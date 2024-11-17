e29c6e2e0cb6495e3a34fe76046d95941459c69e
kokito741 <48101187+kokito741@users.noreply.github.com>
2024-11-17 14:58:26 +0200

    fix sql standart

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 // Database configuration
-$servername = "142.93.163.115";
-$username = "api-injest-php";
-$password = "OVwljsvfSKR5OMoL";
-$dbname = "sensor-data";
+$servername = "your_server_name"; // Replace with your server address or IP
+$username = "your_database_username";
+$password = "your_database_password";
+$dbname = "your_database_name";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -13,27 +13,31 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Get data from request
-$device_id = $_POST['device_id'];
-$temp = $_POST['temp'];
-$humidity = $_POST['humidity'];
-$date_taken = $_POST['date_taken'];
-$device_password = $_POST['device_password'];
+// Check if all necessary POST data is set
+if (isset($_POST['device_id'], $_POST['temp'], $_POST['humidity'], $_POST['date_taken'], $_POST['device_password'])) {
+    $device_id = $_POST['device_id'];
+    $temp = $_POST['temp'];
+    $humidity = $_POST['humidity'];
+    $date_taken = $_POST['date_taken'];
+    $device_password = $_POST['device_password'];
 
-// Validate device
-$sql = "SELECT * FROM devise-list WHERE device_id='$device_id' AND device_password='$device_password'";
-$result = $conn->query($sql);
+    // Validate device
+    $sql = "SELECT * FROM `devices-list` WHERE device_id='$device_id' AND device_password='$device_password'";
+    $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    // Insert data into the database
-    $sql = "INSERT INTO sensor_data.device-sensors (device_id, temp, humidity, date_taken) VALUES ('$device_id', '$temp', '$humidity', '$date_taken')";
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
+    if ($result->num_rows > 0) {
+        // Insert data into the database
+        $sql = "INSERT INTO `sensor_data` (device_id, temp, humidity, date_taken) VALUES ('$device_id', '$temp', '$humidity', '$date_taken')";
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        echo "Invalid device ID or password";
     }
 } else {
-    echo "Invalid device ID or password";
+    echo "Missing required POST data";
 }
 
 $conn->close();

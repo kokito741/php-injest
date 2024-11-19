9de5e592c9d6be5385b696b71a4a11f4d09715b0
kokito741 <48101187+kokito741@users.noreply.github.com>
2024-11-20 01:49:03 +0200

    HJHJ

--- index.php
+++ index.php
@@ -14,12 +14,13 @@ if ($conn->connect_error) {
 }
 
 // Check if all necessary POST data is set
-if (isset($_POST['device_id'], $_POST['temp'], $_POST['humidity'], $_POST['date_taken'], $_POST['device_password'])) {
+if (isset($_POST['device_id'], $_POST['temp'], $_POST['humidity'], $_POST['date_taken'], $_POST['device_password'], $_POST['device_battery'])) {
     $device_id = $_POST['device_id'];
     $temp = $_POST['temp'];
     $humidity = $_POST['humidity'];
     $date_taken = $_POST['date_taken'];
     $device_password = $_POST['device_password'];
+    $device_battery = $_POST['device_battery'];
 
     // Validate device
     $sql = "SELECT * FROM `devise-list` WHERE `device-id`='$device_id' AND `device-password`='$device_password'";
@@ -33,6 +34,14 @@ if (isset($_POST['device_id'], $_POST['temp'], $_POST['humidity'], $_POST['date_
                 echo "New record created successfully";
             } else {
                 echo "Error inserting data: " . $conn->error;
+            }
+
+            // Update the device battery value
+            $sql = "UPDATE `devise-list` SET `device-battery`='$device_battery' WHERE `device-id`='$device_id'";
+            if ($conn->query($sql) === TRUE) {
+                echo "Device battery updated successfully";
+            } else {
+                echo "Error updating device battery: " . $conn->error;
             }
         } else {
             echo "Invalid device ID or password";
